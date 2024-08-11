67f5b5ab32c5249b96fb74e4f5852e5e10445226
Alexander Lapshenkov <gn.abbat@vk.com>
2024-08-11 17:05:40 +0300

    Финальная версия проекта "Галлерея с картинками".

--- JavaScript/module_9_HW-04/main.js
+++ JavaScript/module_9_HW-04/main.js
@@ -1,8 +1,9 @@
 const imagesGalleryDiv = document.querySelector('.gallery');
 const loader = document.querySelector('.gallery__loader');
+const loadButton = document.querySelector('.load-button')
 const imgsLoadErrorSpan = document.createElement('span')
 
-const loaderVisibility = (strStyleDisplayValue) => loader.style.display = strStyleDisplayValue;
+const loaderVisibility = (styleDisplayValueStr) => loader.style.display = styleDisplayValueStr;
 
 loaderVisibility('none');
 
@@ -10,36 +11,60 @@ const imagesLoader = async function () {
     try{
         const URL = 'https://api.thecatapi.com/v1/images/search?limit=10';
         const response = await fetch(URL);
+
+        if(!response.ok){
+            throw new Error (`Response status: ${response.status}`)
+        }
         const data = await response.json();
 
-        let arrImagesURLs = data.map(elem => elem.url);
-
-        arrImagesURLs.map((url) => {
-            return imagesGalleryDiv.appendChild(document.createElement('img')).src = url;
-        });
-
-    } catch (e) {
-        console.log('Developer: "imagesLoader error"');
-        console.error(e.message);
-        loaderVisibility('none');
-        galleryImgsLoadErrorText();
+        let imagesURLsArr = data.map(elem => elem.url);
+        await Promise.all(addAllImagesToGallery(imagesURLsArr));
+        
+    } catch (error) {
+        console.error(error.message);
+        galleryImagesLoadErrorText();
       }
+    loaderVisibility('none');
+    loadButton.disabled = false;
 }
 
-const galleryImgsLoadErrorText = () => {
+const galleryImagesLoadErrorText = () => {
     imagesGalleryDiv.appendChild(imgsLoadErrorSpan)
     imgsLoadErrorSpan.textContent = 'Извините, не удалось загрузить котиков!';
     imgsLoadErrorSpan.style.textAlign = 'center';
 }
 
 window.onload = function(){
-    document.querySelector('.loadButton').addEventListener('click', () => {
-    loaderVisibility('block');
-    setTimeout(() => loaderVisibility('none'), 0);
+    loadButton.addEventListener('click', () => {
+        loadButton.disabled = true;
+        loaderVisibility('block');
 
-    while (imagesGalleryDiv.firstChild) {
-        imagesGalleryDiv.removeChild(imagesGalleryDiv.firstChild);
-    }
-
-    imagesLoader();
+        const galleryImages = imagesGalleryDiv.querySelectorAll('.gallery > img')
+        for(let img of galleryImages){
+            img.remove();
+        }
+        
+        imagesLoader();
 })};
+
+// addAllImagesToGallery returns array of promises
+function addAllImagesToGallery(URLsArr) {
+    return URLsArr.map((url) => asyncAddImageToGallery(url));
+}
+
+function asyncAddImageToGallery(url) {
+    return new Promise((resolve, reject) => {
+        const galleryImg = document.createElement('img');
+        imagesGalleryDiv.appendChild(galleryImg);
+        galleryImg.onload = () => {
+            resolve();
+        }
+        galleryImg.onerror = () => {
+            galleryImg.alt = 'Котик недоступен';
+            galleryImg.style.textAlign = 'center';
+            resolve();
+        }
+        galleryImg.src = url;
+    });
+
+}
