1702d0188f5506904f305bff065063eb924ec23f
Alexander Lapshenkov <gn.abbat@vk.com>
2024-09-01 17:22:29 +0300

    Удалила песочницу и добавила папку для проектов на React.

--- JavaScript/sandbox/7-6-2_exercise.js
+++
@@ -1,5 +0,0 @@
-const someArr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ];
-const someSet = new Set(someArr);
-const uniqueArr = Array.from(someSet);
-
-alert(uniqueArr);
---
+++ React/first-react-app/src/App.jsx
@@ -0,0 +1,30 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function ImageContainer() {
+  return (
+    <img src='https://blog.skillfactory.ru/wp-content/uploads/2023/01/c4bdc760-4b0b-4dd1-8631-6f9544906ca9-5431637.png' alt='Изображение для статьи'/>)
+}
+
+export default ImageContainer;
